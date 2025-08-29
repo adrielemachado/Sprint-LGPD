ab437fae6eb352c85056d119c14e69b528bcf20d
Adriele Machado Moreira <machado.adriele96@gmail.com>
2025-08-29 10:06:17 -0300

    alterações contato, forms, form css

--- js/form.js
+++ js/form.js
@@ -2,18 +2,36 @@
 //class contato
 
 class contato {
-    constructor(nome,email, telefone, tipo, mensagem){
+    constructor(nome,email, telefone, tipo, mensagem,aceitouNewsletter, aceitouTermos){
 
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
         this.tipo = tipo;
         this.mensagem = mensagem;
+        this.aceitouNewsletter = aceitouNewsletter;
+        this.aceitouTermos = aceitouTermos;
+
     }
 
 }
 
+function validarEnvio(){
+    const TermosCheckbox = document.getElementById("TermosCheckbox");
+    const enviarBtn = document.getElementById("enviarBtn");
+
+    enviarBtn.disabled = TermosCheckbox.checked;
+}
+
 function Post(form) {
+
+    const TermosCheckbox = document.getElementById("TermosCheckbox");
+    const newsletterCheckbox = document.getElementById("newsletter");
+
+    if(!TermosCheckbox.checked){
+        alert("Por favor aceite os termos e condições para enviar o formulário");
+        return false;
+    }
 
   let data = new contato (
       form.elements.namedItem("nome").value,
@@ -21,11 +39,23 @@ function Post(form) {
       form.elements.namedItem("telefone").value, 
       form.elements.namedItem("tipo").value,
       form.elements.namedItem("mensagem").value);
+      TermosCheckbox.checked,
+      newsletterCheckbox.checked
+    
 
       console.log("dados do formulário:", data);
 
       alert(`Obrigado, ${data.nome}! Sua mensagem de ${data.tipo} foi enviada com sucesso.`);
 
+      termosCheckbox.checked = false;
+      newsletterCheckbox = false;
+
+      validarEnvio();
+
       form.reset();
 }
 
+
+document.addEventListener("DOMContentLoaded", function(){
+    validarEnvio();
+})
